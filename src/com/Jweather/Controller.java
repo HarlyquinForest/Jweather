5cd89f9ade073528d0931510a0917ca15902ecfe
HarlyquinForest <aminyousefnejad28@gmail.com>
2019-01-31 00:06:21 +0330

    A little work of background wallpaper

--- src/com/Jweather/Controller.java
+++ src/com/Jweather/Controller.java
@@ -1,4 +1,21 @@
 package com.Jweather;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class Controller
+{
+
+    public void NextCityMouseHandler(MouseEvent event)
+    {
+        //Main.scene.getStylesheets().remove(0);
+        //Main.scene.getStylesheets().add(ThemeSummer);
+    }
+    public void PrivCityMouseHandler(MouseEvent event)
+    {
+        //Main.scene.getStylesheets().remove(0);
+        //Main.scene.getStylesheets().add(ThemeFall);
+    }
+
 }
--- src/com/Jweather/Main.java
+++ src/com/Jweather/Main.java
@@ -4,18 +4,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 public class Main extends Application {
 
+    private String ThemeSummer = getClass().getResource("/source/summer.css").toExternalForm();
+    private String ThemeFall = getClass().getResource("/source/fall.css").toExternalForm();
+    private String ThemeWinter = getClass().getResource("/source/winter.css").toExternalForm();
+    private String ThemeSpring = getClass().getResource("/source/spring.css").toExternalForm();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root,1500,750);
+
         primaryStage.setTitle("Jweather");
-        primaryStage.setScene(new Scene(root,1500,750));
+        primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
