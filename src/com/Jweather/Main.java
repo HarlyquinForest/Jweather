c61d48af8bea44c0ed659e4de6f0f990770fdc16
amin <aminyousefnejad28@gmail.com>
2019-01-31 11:04:18 +0330

    Cleaned out /out

--- src/com/Jweather/Main.java
+++ src/com/Jweather/Main.java
@@ -11,14 +11,14 @@ public class Main extends Application {
 
     private String ThemeSummer = getClass().getResource("/source/summer.css").toExternalForm();
     private String ThemeFall = getClass().getResource("/source/fall.css").toExternalForm();
-    private String ThemeWinter = getClass().getResource("/source/winter.css").toExternalForm();
-    private String ThemeSpring = getClass().getResource("/source/spring.css").toExternalForm();
+    //private String ThemeWinter = getClass().getResource("/source/winter.css").toExternalForm();
+    //private String ThemeSpring = getClass().getResource("/source/spring.css").toExternalForm();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         Scene scene = new Scene(root,1500,750);
-
+        scene.getStylesheets().add(ThemeFall);
         primaryStage.setTitle("Jweather");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
