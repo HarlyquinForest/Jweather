f625cf4d76aacec04a6a05648f902ee929092211
Think pad <aminyousefnejad28@gmail.com>
2019-08-22 13:19:28 +0430

    forgot to stage City
    I had forgotten to stag this

--- src/com/Jweather/City.java
+++ src/com/Jweather/City.java
@@ -4,12 +4,35 @@ public class City
 {
     private String name ;
     private String country;
-    private int id ;
+    private int id = 0 ;
 
+    public int getId() {
+        return id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString()
     {
-        return "City name :"  + name + " , Country :" + country + " , id:"+id;
+        return id + "," + name + ","+country;
     }
 }
