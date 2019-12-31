b822ef2d093a4e86d2c6fadc8aebbfd452cc738f
Amin <Yousefnejad>
2019-12-31 19:06:22 +0330

    commit for checkout

--- src/com/Jweather/Settings.java
+++ src/com/Jweather/Settings.java
@@ -2,6 +2,11 @@ package com.Jweather;
 
 public class Settings
 {
-    
+                         //http://api.openweathermap.org/data/2.5/forecast/daily?id=cityID&APPID=04ed4038994ff1be56247052ae7bc45f&units=imperial&mode=xml
+    private String API ; //http://api.openweathermap.org/data/2.5/forecast/daily?id=cityID&APPID=04ed4038994ff1be56247052ae7bc45f&units=metric&mode=xml
+    private String HOME_PATH ;
+    private String CONFIG_PATH ;
+    private City defautlCity ;
+
 
 }
