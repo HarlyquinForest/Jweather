84eaefc63c77edc33e7e61587285ce214eb81e3f
Amin <Yousefnejad>
2020-01-06 01:03:31 +0330

    Created a main thread

--- src/com/Jweather/Main.java
+++ src/com/Jweather/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.stage.Stage;
 import javafx.event.EventHandler;
 import javafx.stage.WindowEvent;
@@ -16,12 +17,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        Scene scene = new Scene(root,1500,750);
-
-        System.out.println("Connected to net ="+Connection.checkConnection());
-
-        Wallpaper wallpaper = new Wallpaper();
-        wallpaper.setWallpaper(scene);
+        Scene scene = new Scene(root,1500,750 );
 
         primaryStage.setTitle("Jweather");
         primaryStage.setScene(scene);
@@ -30,6 +26,19 @@ public class Main extends Application {
         primaryStage.setMinWidth(1500);
         primaryStage.show();
 
+        Wallpaper wallpaper = new Wallpaper();
+        wallpaper.setWallpaper(scene);
+        new Thread(()->
+        {
+            Connection connection = new Connection();
+            connection.start();
+            System.out.println("Main thread started");
+            System.out.println("Connected to net ="+connection.ok);
+            Settings settings = new Settings() ;
+            GetWeatherInfo getWeatherInfo = new GetWeatherInfo(Settings.defaultCity , settings.getAPI());
+
+            System.out.println("Main thread finished");
+        }).start();
 
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
