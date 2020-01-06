bcf6e5f177c4ceb11eb2938288db902996aea7fd
Amin <Yousefnejad>
2020-01-06 11:15:47 +0330

    Linechart compeleted and works almost fine

--- src/com/Jweather/LineChart.java
+++ src/com/Jweather/LineChart.java
@@ -6,16 +6,15 @@ import javafx.scene.shape.Line;
 import javafx.scene.paint.Color;
 import javafx.scene.control.Label;
 
-import java.awt.*;
-
 public class LineChart
 {
     private Pane layout ;
+    private Line[] bars;
     private Line[] lines ;
     private Circle[] circles ;
     private Label[] tempertures;
     private Label[] times;
-    private String[] labels ;
+    private String[] x_axis_label;
     private int[] coor ;
     private int margin = 10 ;
     private int pading = 10 ;
@@ -23,17 +22,20 @@ public class LineChart
     private int[] x2 ;
     private int[] y1 ;
     private int[] y2;
-    int x_distance ;
-    int y_distance ;
+    private int x_distance ;
+    private int y_distance ;
 
     LineChart(Pane pane , int[] x , String[] labels)
     {
         layout = pane ;
         coor = x ;
-        this.labels = labels;
+        x_axis_label = labels;
         int len = coor.length;
+        bars = new Line[len];
         lines = new Line[len];
         times = new Label[len];
+        circles = new Circle[len];
+        tempertures = new Label[len];
         x1 = new int[len];
         x2 = new int[len];
         y1 = new int[len];
@@ -42,31 +44,55 @@ public class LineChart
 
     boolean drawChart()
     {
+        layout.getChildren().clear();
         dataBinding();
         int x = 0 ;
         int layoutHeight = (int)layout.getHeight();
         for(int i = 0 ; i < coor.length ; i++)
         {
-            lines[i] =  new Line();
-            lines[i].setEndX(x_distance-margin);
-            lines[i].setEndY(-(layoutHeight - (margin + pading)));
-            lines[i].setStartX(x_distance-margin);
-            lines[i].setStartY(0 - pading*2);
-            lines[i].setLayoutY(layoutHeight - margin);
-            lines[i].setLayoutX(x);
-            lines[i].setStroke(Color.WHITE);
-            lines[i].setOpacity(1.0);
-            layout.getChildren().add(lines[i]);
+            bars[i] =  new Line(x_distance-margin , 0-pading*2 , x_distance-margin ,-(layoutHeight - (margin + pading)) );
+            bars[i].setLayoutY(layoutHeight - margin);
+            bars[i].setLayoutX(x);
+            bars[i].setStroke(Color.WHITE);
+            bars[i].setOpacity(0.3);
 
             x+=x_distance;
 
-            times[i] = new Label(labels[i]);
+            times[i] = new Label(x_axis_label[i]);
             times[i].setPrefWidth(50);
             times[i].setLayoutX(x - 20);
             times[i].setLayoutY(layoutHeight - (margin + pading));
             times[i].setTextFill(Color.WHITE);
+
+            lines[i] = new Line(x1[i],y1[i],x2[i],y2[i]);
+            lines[i].setStroke(Color.WHITE);
+            lines[i].setStrokeWidth(2);
+            lines[i].setOpacity(0.5);
+
+            circles[i] = new Circle();
+            circles[i].setRadius(3);
+            if(i == 0 )
+                circles[i].setCenterX(x_distance-margin);
+            else
+                circles[i].setCenterX(x2[i-1]);
+            circles[i].setCenterY(y1[i]);
+            circles[i].setFill(Color.WHITE);
+
+            tempertures[i] = new Label(coor[i]+"");
+            tempertures[i].setTextFill(Color.WHITE);
+            if(i == 0 )
+                tempertures[i].setLayoutX(x_distance - margin);
+            else
+                tempertures[i].setLayoutX(x2[i - 1] - 10);
+            tempertures[i].setLayoutY(y1[i] - 30);
+
+            layout.getChildren().add(circles[i]);
+            layout.getChildren().add(bars[i]);
             layout.getChildren().add(times[i]);
+            layout.getChildren().add(lines[i]);
+            layout.getChildren().add(tempertures[i]);
         }
+
         return true;
     }
     void dataBinding()
@@ -75,14 +101,20 @@ public class LineChart
         x_distance =(int) (layout.getWidth() / length) - margin ;
         y_distance =(int) layout.getHeight() / 2 ;
         // x2 = x1 + length
-        x1[0] = 0 ;
+        System.out.println("Coordinates");
+        x1[0] = x_distance - margin ;
         for(int i = 0 ; i < coor.length ; i++)
         {
-            y2[i] = y_distance - coor[i];
-            y1[i] = y2[i] - y_distance;
-
+            if(i+1 < coor.length)
+                y2[i] = y_distance - coor[i+1];
+            else
+                y2[i] = y_distance - coor[i];
+            y1[i] = y_distance - coor[i];
+            if(i - 1 >= 0 )
+            {
+                x1[i] = x1[i - 1] + x_distance;
+            }
             x2[i] = x1[i] + x_distance;
-            x1[i] = x2[i] - x_distance;
         }
 
     }
